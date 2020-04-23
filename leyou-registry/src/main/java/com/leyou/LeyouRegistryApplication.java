111e1b870c820be5051257bf75895b91d0c814a0
JingZiLa <1439268312@qq.com>
2020-04-23 22:28:07 +0800

    注册中心

---
+++ leyou-registry/src/main/java/com/leyou/LeyouRegistryApplication.java
@@ -0,0 +1,19 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author Mirror
+ * @CreateDate 2020/3/12.
+ * eureka引导类
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class LeyouRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouRegistryApplication.class,args);
+    }
+}
